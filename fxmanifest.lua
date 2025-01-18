1bcc1b40c410e26b65e4bb1e1de0b64dfce5840f
1stSlacker <xdtrayray@gmail.com>
2025-01-17 18:24:09 -0800

    An advanced disable radio script for QB-Core

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Slacker'
+description 'An advanced disable radio script for QB-Core'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua',
+}
+
+lua54 'yes'
